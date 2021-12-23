67ba53d815163853a076b8e0d3a8d9e105792e43
Aiden <aiden.gimpel@ontariotechu.net>
2021-12-22 20:15:37 -0500

    Added an alert for when the crystal is at half health

--- Chibi Champions/Assets/Scripts/Crystal.cs
+++ Chibi Champions/Assets/Scripts/Crystal.cs
@@ -6,12 +6,15 @@ using TMPro;
 
 public class Crystal : MonoBehaviour
 {
-
     TextMeshProUGUI healthText;
+    [SerializeField] TextMeshProUGUI alertText;
+    bool alertFired;
 
     private void Start()
     {
         healthText = GetComponentInChildren<TextMeshProUGUI>();
+
+        alertText.gameObject.SetActive(false);
     }
     // Update is called once per frame
     void Update()
@@ -19,5 +22,11 @@ public class Crystal : MonoBehaviour
         healthText.text = GetComponent<Health>().GetCurrentHealth() + "/" + GetComponent<Health>().GetMaxHealth();
 
         transform.RotateAround(transform.position, new Vector3(0, 1, 0), 0.1f);
+
+        if (GetComponent<Health>().GetCurrentHealth() <= 50 && !alertFired)
+        {
+            alertText.gameObject.SetActive(true);
+            alertFired = true;
+        }
     }
 }

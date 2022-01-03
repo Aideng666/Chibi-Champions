a53b40486efd8c425bb5b2a36996e0b08d14a808
Tyler <tyler.wong@ontariotechu.net>
2022-01-03 18:43:45 -0500

    Added mode select assets

--- Chibi Champions/Assets/Scripts/Menus/ModeSelectMenu.cs
+++ Chibi Champions/Assets/Scripts/Menus/ModeSelectMenu.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ModeSelectMenu : MonoBehaviour
 {
+    public Button backButton;
+
     public void OnClick_Back()
     {
         MenuManager.OpenMenu(Menu.MAIN_MENU, gameObject);
+        backButton.transform.localScale = new Vector3(1, 1, 1);
     }
 
     public void OnClick_SinglePlayer()

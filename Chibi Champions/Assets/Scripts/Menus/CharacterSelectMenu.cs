e4a9d40d3a4b23a56790d0f0020a389ed706e883
Tyler <tyler.wong@ontariotechu.net>
2022-01-03 20:44:52 -0500

    started character select UI menu

--- Chibi Champions/Assets/Scripts/Menus/CharacterSelectMenu.cs
+++ Chibi Champions/Assets/Scripts/Menus/CharacterSelectMenu.cs
@@ -3,67 +3,72 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityEngine.UI;
 
 public class CharacterSelectMenu : MonoBehaviour
 {
-    [SerializeField]
-    GameObject[] characters;
+    //[SerializeField]
+    //GameObject[] characters;
 
-    [SerializeField]
-    TMP_Text[] UIText;
+    //[SerializeField]
+    //TMP_Text[] UIText;
 
-    private int characterIndex;
+    //private int characterIndex;
 
-    bool hasSelectedCharacter = false;
+    //bool hasSelectedCharacter = false;
 
-    public CharacterDatabase characterDB;
-    public TMP_Text characterName;
-    public TMP_Text characterType;
+    //public CharacterDatabase characterDB;
+    //public TMP_Text characterName;
+    //public TMP_Text characterType;
 
-    public void ChangeCharacter(int index)
-    {
-        for(int i = 0; i < characters.Length; ++i)
-        {
-            characters[i].SetActive(false);
-        }
+    public Button backButton, lockInButton;
 
-        for(int i = 0; i < UIText.Length; ++i)
-        {
-            UIText[i].text = "";
-        }
-
-        characterIndex = index;
-
-        characters[index].SetActive(true);
-
-        Character character = characterDB.GetCharacter(index);
-        characterName.text = character.characterName;
-        characterType.text = character.characterType;
-
-        hasSelectedCharacter = true;
-    }
+    //public void ChangeCharacter(int index)
+    //{
+    //    for(int i = 0; i < characters.Length; ++i)
+    //    {
+    //        characters[i].SetActive(false);
+    //    }
+    //
+    //    for(int i = 0; i < UIText.Length; ++i)
+    //    {
+    //        UIText[i].text = "";
+    //    }
+    //
+    //    characterIndex = index;
+    //
+    //    characters[index].SetActive(true);
+    //
+    //    Character character = characterDB.GetCharacter(index);
+    //    characterName.text = character.characterName;
+    //    characterType.text = character.characterType;
+    //
+    //    hasSelectedCharacter = true;
+    //}
 
 
     public void OnClick_Back()
     {
         MenuManager.OpenMenu(Menu.MODE_SELECT, gameObject);
-        for (int i = 0; i < characters.Length; ++i)
-        {
-            characters[i].SetActive(false);
-        }
+        backButton.transform.localScale = new Vector3(1, 1, 1);
 
-        for (int i = 0; i < UIText.Length; ++i)
-        {
-            UIText[i].text = "";
-        }
+        //for (int i = 0; i < characters.Length; ++i)
+        //{
+        //    characters[i].SetActive(false);
+        //}
+        //
+        //for (int i = 0; i < UIText.Length; ++i)
+        //{
+        //    UIText[i].text = "";
+        //}
     }
 
-    public void OnClickPlay()
-    {
-        if (hasSelectedCharacter)
-        {
-            SceneManager.LoadScene("Sandbox");
-            PlayerPrefs.SetInt("CharacterIndex", characterIndex);
-        } 
-    }
+    //public void OnClickPlay()
+    //{
+    //    if (hasSelectedCharacter)
+    //    {
+    //        SceneManager.LoadScene("Sandbox");
+    //        PlayerPrefs.SetInt("CharacterIndex", characterIndex);
+    //    } 
+    //}
 }

8e52f6cfd91d5edd9ada479b2dab435945d5bef0
Tyler <tyler.wong@ontariotechu.net>
2022-02-22 17:08:32 -0500

    Added Play and Options

--- Chibi Champions/Assets/Scripts/Menus/MenuManagerV2.cs
+++ Chibi Champions/Assets/Scripts/Menus/MenuManagerV2.cs
@@ -7,8 +7,8 @@ using UnityEngine.EventSystems;
 public class MenuManagerV2 : MonoBehaviour
 {
     public CinemachineBrain mainCamera;
-    public CinemachineVirtualCamera frame0Cam;
-    public CinemachineVirtualCamera frame1Cam;
+
+    public CinemachineVirtualCamera[] frameCams;
 
     public GameObject[] frame;
     public GameObject startButton;
@@ -23,15 +23,17 @@ public class MenuManagerV2 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Main Menu Base
         if (Input.anyKeyDown && frame[0].activeInHierarchy)
         {
             frame[0].SetActive(false);
             frame[1].SetActive(true);
-            ES.SetSelectedGameObject(startButton);
-            frame0Cam.gameObject.SetActive(false);
-            frame1Cam.gameObject.SetActive(true);
+            //ES.SetSelectedGameObject(startButton);
+            frameCams[0].gameObject.SetActive(false);
+            frameCams[1].gameObject.SetActive(true);
         }
 
+        // Pressing ESC goes all the way back to the main menu base (Frame0)
         if (Input.GetKeyDown(KeyCode.Escape) && !frame[0].activeInHierarchy)
         {
             for (int i = 1; i < frame.Length; ++i)
@@ -41,8 +43,57 @@ public class MenuManagerV2 : MonoBehaviour
 
             frame[0].SetActive(true);
 
-            frame1Cam.gameObject.SetActive(false);
-            frame0Cam.gameObject.SetActive(true);
+            for(int i = 1; i < frameCams.Length; ++i)
+            {
+                frameCams[i].gameObject.SetActive(false);
+            }
+
+            frameCams[0].gameObject.SetActive(true);
         }
+    }
+
+    // FRAME 1 STUFF
+
+    public void Play()
+    {
+        frame[1].SetActive(false);
+        frame[2].SetActive(true);
+        frameCams[1].gameObject.SetActive(false);
+        frameCams[2].gameObject.SetActive(true);
+    }
+
+    public void Options()
+    {
+        frame[1].SetActive(false);
+        frame[3].SetActive(true);
+        frameCams[1].gameObject.SetActive(false);
+        frameCams[3].gameObject.SetActive(true);
+    }
+
+    // Calls the quit function
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    // Back Button Functions
+    public void BackButtonCurrentFrame(GameObject frame)
+    {
+        frame.SetActive(false);
+    }
+
+    public void BackButtonTargetFrame(GameObject frame)
+    {
+        frame.SetActive(true);
+    }
+
+    public void BackButtonCurrentCam(CinemachineVirtualCamera cam)
+    {
+        cam.gameObject.SetActive(false);
+    }
+
+    public void BackButtonTargetCam(CinemachineVirtualCamera cam)
+    {
+        cam.gameObject.SetActive(true);
     }
 }

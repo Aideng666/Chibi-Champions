3c49b8f0548d064c62be2930710d27d21c9d6283
Tyler <tyler.wong@ontariotechu.net>
2022-03-15 17:35:38 -0400

    Added some panel transitions

--- Chibi Champions/Assets/Scripts/Menus/MenuController.cs
+++ Chibi Champions/Assets/Scripts/Menus/MenuController.cs
@@ -6,24 +6,38 @@ public class MenuController : MonoBehaviour
 {
     public GameObject[] frames;
 
+    //public Animator fadeTransitionAnim;
+    //public float fadeTransitionTime = 1.5f;
+
+    //public Animator panelTransitionAnim;
+    //public float wipeTransitionTime = 2f;
+
     // Update is called once per frame
     void Update()
-    {
+    {    
         if (Input.anyKeyDown && frames[0].activeInHierarchy)
         {
             frames[0].SetActive(false);
             frames[1].SetActive(true);
             FindObjectOfType<AudioManager>().Play("Click");
+            //StartCoroutine(LoadFadeTransition());
+            //StartCoroutine(LoadTransition());
         }
-
-        //if (Input.GetKeyDown(KeyCode.Escape) && !frames[0].activeInHierarchy)
-        //{
-        //    for (int i = 1; i < frames.Length; ++i)
-        //    {
-        //        frames[i].SetActive(false);
-        //    }
-
-        //    frames[0].SetActive(true);
-        //}
     }
+
+    //IEnumerator LoadFadeTransition()
+    //{
+    //    fadeTransitionAnim.SetTrigger("End");
+    //    yield return new WaitForSeconds(fadeTransitionTime);
+    //    frames[0].SetActive(false);
+    //    frames[1].SetActive(true);
+    //}
+
+    //IEnumerator LoadTransition()
+    //{
+    //    panelTransitionAnim.SetTrigger("Start");
+    //    yield return new WaitForSeconds(wipeTransitionTime);
+    //    frames[0].SetActive(false);
+    //    frames[1].SetActive(true);
+    //}
 }

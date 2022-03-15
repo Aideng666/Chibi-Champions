952241008d15b364b7364fd51439b26897555226
Aiden <aiden.gimpel@ontariotechu.net>
2022-03-14 21:13:03 -0400

    Lobby Messaging Works

--- Chibi Champions/Assets/Scripts/Networking/Server.cs
+++ Chibi Champions/Assets/Scripts/Networking/Server.cs
@@ -5,76 +5,112 @@ using System;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
+using TMPro;
 
 public class Server : MonoBehaviour
 {
-    //[SerializeField] GameObject cube;
-    string message;
+    TMP_InputField input;
+    string receivedMessage;
+    string messageToSend;
 
     private static byte[] buffer;
+    private static byte[] message;
     private static IPHostEntry hostInfo;
     private static IPAddress ip;
     private static IPEndPoint localEP;
     private static Socket server;
     private static IPEndPoint client;
-    private static EndPoint remoteClient;
     private static int rec = 0;
-
-    //Lecture 5
-    //private float[] pos;
+    private static Socket handler;
 
     private static bool serverRunning;
 
+    //string previousMessage;
+
     void Start()
     {
+        input = FindObjectOfType<TMP_InputField>();
+
+        input.text = "Return";
+
         StartServer();
 
-        //Lecture 5
         //Non-Blocking Mode
         server.Blocking = false;
+        handler.Blocking = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        rec = 0;
+
+        if (handler == null)
+        {
+            print("Waiting To Reconnect...");
+
+            handler = server.Accept();
+
+            print("Successfully Reconnected!");
+
+            client = (IPEndPoint)handler.RemoteEndPoint;
+
+            print($"Client {client.Address} connected at port {client.Port}");
+
+            handler.Blocking = false;
+
+            serverRunning = true;
+        }
+
         if (serverRunning)
         {
             try
             {
-                rec = server.ReceiveFrom(buffer, ref remoteClient);
+                rec = handler.Receive(buffer);
             }
             catch (SocketException e)
+            {
+                print(e.SocketErrorCode);
+
+                if (e.SocketErrorCode == SocketError.ConnectionReset)
+                {
+                    handler = null;
+                    serverRunning = false;
+                }
+            }
+            catch (Exception e)
             {
                 print(e.ToString());
             }
 
-            print($"Received from: {remoteClient.ToString()}");
+            if (rec > 0)
+            {
+                receivedMessage = Encoding.ASCII.GetString(buffer, 0, rec);
 
-            message = Encoding.ASCII.GetString(buffer, 0, rec);
+                Debug.Log("Recieved: " + Encoding.ASCII.GetString(buffer, 0, rec));
 
-            LobbyManager.Instance.SetMessage(message);
+                if (receivedMessage != "NO:MESSAGE/SENT.KEY")
+                {
+                    LobbyManager.Instance.SetMessage(receivedMessage);
+                }
+            }
 
-            print(message);
+            if (input.text.Length > 0)
+            {
+                messageToSend = input.text;
+            }
+            else
+            {
+                messageToSend = "NO:MESSAGE/SENT.KEY";
+            }
 
-            //LECTURE 5
-            //pos = new float[rec / 4];
+            message = Encoding.ASCII.GetBytes(messageToSend);
 
-            //Buffer.BlockCopy(buffer, 0, pos, 0, rec);
-
-            //cube.transform.position = new Vector3((float)Convert.ToDouble(xPos), 0.5f, (float)Convert.ToDouble(zPos));
-            //cube.transform.position = new Vector3(pos[0], pos[1], pos[2]);
-
-            //print($"Received Position: {cube.transform.position}");
-
-            // If client sends floats
-            // print($"Data: {BitConverter.ToSingle(buffer, 0)}");
+            //if (message.Length > 0)
+            //{
+            //    handler.Send(message);
+            //}
         }
-
-        //if (cube.transform.position.x > 5 || cube.transform.position.x < -5 ||
-        //    cube.transform.position.z > 5 || cube.transform.position.z < -5)
-        //{
-        //    ShutdownServer();
-        //}
     }
 
     public static void StartServer()
@@ -87,15 +123,22 @@ public class Server : MonoBehaviour
         print($"Server Name: {hostInfo.HostName} | IP: {ip}");
 
         localEP = new IPEndPoint(ip, 11111);
-        server = new Socket(ip.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
-        client = new IPEndPoint(IPAddress.Any, 0);
-        remoteClient = (EndPoint)client;
+        server = new Socket(ip.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
         try
         {
             server.Bind(localEP);
+            server.Listen(10);
 
-            print("Waiting for data...");
+            print("Waiting for Connection...");
+
+            handler = server.Accept();
+
+            print("Connected!");
+
+            client = (IPEndPoint)handler.RemoteEndPoint;
+
+            print($"Client {client.Address} connected at port {client.Port}");
         }
         catch (Exception e)
         {
@@ -105,12 +148,37 @@ public class Server : MonoBehaviour
         serverRunning = true;
     }
 
+    public void ActivateSendMessage()
+    {
+        SendMessage();
+    }
+
+    public static void SendMessage()
+    {
+        handler.Send(message);
+    }
+
     public void ShutdownServer()
     {
         server.Shutdown(SocketShutdown.Both);
         server.Close();
 
         serverRunning = false;
+    }
+
+    void WaitForSendMessage(KeyCode key)
+    {
+        bool done = false;
+
+        while (!done)
+        {
+            print("Waiting For Input");
+
+            if (Input.GetKeyDown(key))
+            {
+                done = true;
+            }
+        }
     }
 }
 

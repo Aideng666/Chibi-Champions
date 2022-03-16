626e5ba1bbcb84f945571853e8ee9bd0d431f83a
Tyler <tyler.wong@ontariotechu.net>
2022-03-15 23:20:51 -0400

    Still figuring out fade

--- Chibi Champions/Assets/Scripts/Menus/MenuController.cs
+++ Chibi Champions/Assets/Scripts/Menus/MenuController.cs
@@ -6,41 +6,29 @@ public class MenuController : MonoBehaviour
 {
     public GameObject[] frames;
 
-    public Animator startfadeTransitionAnim;
-    public float startfadeTransitionTime = 1.5f;
+    //public Animator fadeTransitionAnim;
+    //public float fadeTransitionTime = 1.5f;
 
     //public Animator panelTransitionAnim;
     //public float wipeTransitionTime = 2f;
 
     // Update is called once per frame
     void Update()
-    {    
+    {
         if (Input.anyKeyDown && frames[0].activeInHierarchy)
         {
-            //frames[0].SetActive(false);
-            //frames[1].SetActive(true);
+            frames[0].SetActive(false);
+            frames[1].SetActive(true);
             FindObjectOfType<AudioManager>().Play("Click");
-            StartCoroutine(LoadFadeTransition());
-            //StartCoroutine(LoadFadeTransition(frames[0], frames[1]));
             //StartCoroutine(LoadFadeTransition());
             //StartCoroutine(LoadTransition());
         }
     }
 
-    IEnumerator LoadFadeTransition()
-    {
-        startfadeTransitionAnim.SetTrigger("End");
-        yield return new WaitForSeconds(startfadeTransitionTime);
-        frames[0].SetActive(false);
-        frames[1].SetActive(true);
-    }
-
-    //IEnumerator LoadFadeTransition(/*GameObject currentFrame, GameObject targetFrame*/)
+    //IEnumerator LoadFadeTransition()
     //{
     //    fadeTransitionAnim.SetTrigger("End");
     //    yield return new WaitForSeconds(fadeTransitionTime);
-    //    //currentFrame.SetActive(false);
-    //    //targetFrame.SetActive(true);
     //    frames[0].SetActive(false);
     //    frames[1].SetActive(true);
     //}

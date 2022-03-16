950174bfc0e087d2ff5ee1870460e6e6391e0625
Tyler <tyler.wong@ontariotechu.net>
2022-03-16 13:54:48 -0400

    Playing with button anims

--- Chibi Champions/Assets/Scripts/Menus/AnimTestScript.cs
+++ Chibi Champions/Assets/Scripts/Menus/AnimTestScript.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class AnimTestScript : MonoBehaviour
 {
+    public GameObject[] frames;
+
     public Animator anim;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && frames[0].activeInHierarchy)
         {
             Debug.Log("PRESSED");
+            frames[0].SetActive(false);
+            frames[1].SetActive(true);
             anim.SetTrigger("Clicked");
         }
     }

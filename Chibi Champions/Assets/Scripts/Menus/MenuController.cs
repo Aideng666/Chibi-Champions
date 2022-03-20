f0756c61a80ef55461d9cf58e4320e9ca3fc5e09
Tyler <tyler.wong@ontariotechu.net>
2022-03-19 20:38:26 -0400

    Fixed fade transition

--- Chibi Champions/Assets/Scripts/Menus/MenuController.cs
+++ Chibi Champions/Assets/Scripts/Menus/MenuController.cs
@@ -36,7 +36,7 @@ public class MenuController : MonoBehaviour
             StartCoroutine(LoadFadeTransitions(frames[1], frames[2]));
             isPlayButtonClicked = false;
         }
-        if (isTutorialButtonClicked)
+        else if (isTutorialButtonClicked)
         {
             StartCoroutine(LoadFadeTransitions(frames[1], frames[3]));
             isTutorialButtonClicked = false;
